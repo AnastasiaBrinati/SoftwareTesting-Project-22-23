1b56e8da44297c17947dd3b636ae6ca24e1dc110
Anastasia <anastasia.brinati@students.uniroma2.eu>
2023-02-28 11:11:05 +0100

    TDD: this build is going to fail; added tests to testSuite

--- src/test/java/LogicTestSuite.java
+++ src/test/java/LogicTestSuite.java
@@ -2,7 +2,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestSubtraction.class, TestAddition.class})
+@Suite.SuiteClasses({TestSubtraction.class, TestAddition.class, TestMultiplication.class, TestDivision.class})
 
 public class LogicTestSuite {
 }
