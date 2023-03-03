0992f0325ca66bf3c0794a6205203c6bb55056a4
Giulio <giulio.appetito.ga@gmail.com>
2023-03-03 22:05:45 +0100

    Methods implemented for add,sub,mul e div

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,18 +3,18 @@ package calculator;
 public class Calculator {
 
     public static double add(double a, double b) {
-        return 0.0;
+        return a+b;
     }
 
     public static double sub(double a, double b) {
-        return 0.0;
+        return a-b;
     }
 
     public static double mul(double a, double b) {
-        return 0.0;
+        return a*b;
     }
 
     public static double div(double a, double b) {
-        return 0.0;
+        return a/b;
     }
 }
