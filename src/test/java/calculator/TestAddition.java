31ca45ad0695c36f43f83a32996abd1c839eb5ef
Giulio <giulio.appetito.ga@gmail.com>
2023-03-12 15:23:09 +0100

    Eye removed

--- src/test/java/calculator/TestAddition.java
+++ src/test/java/calculator/TestAddition.java
@@ -29,22 +29,4 @@ public class TestAddition {
     public void testAddDiv() throws DivisionByZeroException {
         assertEquals(6.0, Calculator.div(Calculator.add(8.0,4.0),2.0), 0.0);
     }
-
-    // GUI testing
-
-    @Test
-    public void eyeTest(){
-        Eye eye = new Eye();
-        BufferedImage image = eye.loadImage("legoLogo.png");
-        if(image != null){
-            Match match = eye.findImage(image);
-            assertNotEquals(null, match);
-            eye.click(match.getCenterLocation());
-        }else{
-            System.out.println("Image is null.");
-        }
-    }
-
-
-
 }
