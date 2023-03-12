e9f6aaf44379b758465c6e1b9b974e51664f5afc
Giulio <giulio.appetito.ga@gmail.com>
2023-03-12 22:44:02 +0100

    Last commit

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application{
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("calculator.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("My Calculator");
+        stage.setTitle("My_Calculator");
         stage.setScene(scene);
         stage.show();
     }
