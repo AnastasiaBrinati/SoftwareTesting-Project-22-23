4378bfaeec3a869dcd6326b757663b800033cac9
Anastasia <anastasia.brinati@students.uniroma2.eu>
2023-03-12 09:56:46 +0100

    TDD: correction on TestExpression.java divisions

--- src/test/java/calculator/TestExpression.java
+++ src/test/java/calculator/TestExpression.java
@@ -7,64 +7,61 @@ import static org.junit.Assert.assertEquals;
 public class TestExpression {
 
     // Integration Testing
+
+    // Subtraction
     @Test
-    public void testSubAdd() throws DivisionByZeroException {
+    public void testSubAdd(){
         assertEquals(11.0, Calculator.add(Calculator.sub(10.0,3.0), 4.0),0);
     }
-
     @Test
     public void testSubMul(){
         assertEquals(12.0, Calculator.mul(Calculator.sub(5.0, 2.0),4.0), 0.0);
     }
-
     @Test
     public void testSubDiv() throws DivisionByZeroException {
         assertEquals(2.0, Calculator.div(Calculator.sub(8.0,4.0),2.0), 0.0);
     }
 
+    // Addition
     @Test
-    public void testAddSub() throws DivisionByZeroException {
+    public void testAddSub(){
         assertEquals(7.0, Calculator.sub(Calculator.add(10.0,4.0), 7.0),0);
     }
-
     @Test
     public void testAddMul(){
         assertEquals(28.0, Calculator.mul(Calculator.add(5.0, 2.0),4.0), 0.0);
     }
-
     @Test
     public void testAddDiv() throws DivisionByZeroException {
         assertEquals(6.0, Calculator.div(Calculator.add(8.0,4.0),2.0), 0.0);
     }
 
+    // Multiplication
     @Test
-    public void testMulSub() throws DivisionByZeroException {
+    public void testMulSub(){
         assertEquals(35.0, Calculator.sub(Calculator.mul(10.0,4.0), 5.0),0);
     }
-
     @Test
     public void testMulAdd(){
         assertEquals(14.0, Calculator.add(Calculator.mul(5.0, 2.0),4.0), 0.0);
     }
-
     @Test
     public void testMulDiv() throws DivisionByZeroException {
         assertEquals(16.0, Calculator.div(Calculator.mul(8.0,4.0),2.0), 0.0);
     }
 
+    // Division
     @Test
     public void testDivSub() throws DivisionByZeroException {
-        assertEquals(7.0, Calculator.sub(Calculator.add(10.0,4.0), 7.0),0);
+        assertEquals(10.0, Calculator.add(Calculator.div(12.0,4.0), 7.0),0);
     }
-
     @Test
-    public void testDivAdd(){
-        assertEquals(28.0, Calculator.mul(Calculator.add(5.0, 2.0),4.0), 0.0);
+    public void testDivAdd() throws DivisionByZeroException{
+        assertEquals(12.0, Calculator.mul(Calculator.div(6.0, 2.0),4.0), 0.0);
     }
-
     @Test
-    public void testDiv() throws DivisionByZeroException {
-        assertEquals(6.0, Calculator.div(Calculator.add(8.0,4.0),2.0), 0.0);
+    public void testDivMul() throws DivisionByZeroException {
+        assertEquals(6.0, Calculator.mul(Calculator.div(12.0,4.0),2.0), 0.0);
     }
 
 }
