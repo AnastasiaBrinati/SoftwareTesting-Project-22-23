a57d38d36334938540606dbb1abb801cbc0c1064
Giulio <giulio.appetito.ga@gmail.com>
2023-03-12 10:58:55 +0100

    Eye tests added

--- src/test/java/calculator/TestAddition.java
+++ src/test/java/calculator/TestAddition.java
@@ -1,15 +1,30 @@
 package calculator;
 
-import javafx.scene.robot.Robot;
+import eye.Eye;
+import eye.Match;
 import org.junit.Test;
+import java.awt.image.BufferedImage;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TestAddition {
 
     @Test
     public void testAdd(){
         assertEquals(7.0, Calculator.add(5.0, 2.0),0);
+    }
+
+    @Test
+    public void eyeTest(){
+        Eye eye = new Eye();
+        BufferedImage image = eye.loadImage("chromelogo.png");
+        if(image != null){
+            Match match = eye.findImage(image);
+            assertNotEquals(null, match);
+            eye.click(match.getCenterLocation());
+        }else{
+            System.out.println("Image is null.");
+        }
     }
 
 }
