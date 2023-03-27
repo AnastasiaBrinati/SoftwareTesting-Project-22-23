bb66d8b78e3b0248b20b2d0cca26390a313c4cef
Giulio <giulio.appetito.ga@gmail.com>
2023-03-28 00:02:54 +0200

    TDD: adding test for new operation to be implemented: pow

--- src/test/java/calculator/LogicTestSuite.java
+++ src/test/java/calculator/LogicTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestSubtraction.class, TestAddition.class, TestMultiplication.class, TestDivision.class})
+@Suite.SuiteClasses({TestSubtraction.class, TestAddition.class, TestMultiplication.class, TestDivision.class, TestPower.class})
 
 public class LogicTestSuite {
 }
